6c6df08d80c7a656d368c65e772424d7ec24ec66
nharris77 <nharris5@nd.edu>
2023-10-12 09:58:31 -0400

    Commit file and data cleaning

--- Interim_Pres.R
+++ Interim_Pres.R
@@ -1,4 +1,19 @@
 # Nick Harris and Max Manyak Interim Presentation
 
-read.csv("~/Downloads/Rail_Equipment_Accident_Incident_Data.csv")
+# Data Clean
 
+library(dplyr)
+
+train_accident <- readr::read_csv("~/Downloads/Rail_Equipment_Accident_Incident_Data.csv")
+
+union_pacific <- train_accident %>%
+  filter(`Reporting Railroad Name` %in% c("Union Pacific Railroad Company"))
+
+norfolk_southern <- train_accident %>%
+  filter(`Reporting Railroad Name` %in% c("Norfolk Southern Railway Company"))
+
+illinois_central <- train_accident %>%
+  filter(`Reporting Railroad Name` %in% c("Illinois Central Railroad Company"))
+
+conrail <- train_accident %>%
+  filter(`Reporting Railroad Name` %in% c("Conrail"))
